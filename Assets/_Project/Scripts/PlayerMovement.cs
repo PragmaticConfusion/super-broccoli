5106a55477d2e0272b4ba7d87a7b69eba91563da
PragmaticConfusion <10893930+PragmaticConfusion@users.noreply.github.com>
2020-07-12 19:06:40 +0530

    new scene for tests

---
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+//    private  player;
+
+    private float speed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f);
+
+    }
+}
