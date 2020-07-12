19401cc42653d4259aa0811477beb2153bb287ce
PragmaticConfusion <10893930+PragmaticConfusion@users.noreply.github.com>
2020-07-12 21:42:05 +0530

    cursed movement

--- Assets/_Project/Scripts/JumpHelper.cs
+++ Assets/_Project/Scripts/JumpHelper.cs
@@ -5,34 +5,18 @@ using UnityEngine;
 public class JumpHelper : MonoBehaviour
 {
     private bool clearance = false;
-    private float defaultJumpPower = 4f; // normal jump power, can be changed for special jump cases
-    public PlayerMovement playerCharacter;
 
-    // Start is called before the first frame update
-    void Start()
+    public bool getJump()
     {
-        //player )
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetButtonDown("Jump") & clearance)
-        {
-            playerCharacter.PlayerJump(defaultJumpPower);
-        }
-    }
-
+        return clearance;
+    }    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         clearance = true;
-
-        Debug.Log("clearance is now true");
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         clearance = false;
-        Debug.Log("clearance is now false");
     }
 }
--- Assets/_Project/Scripts/PlayerMovement.cs
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -6,24 +6,94 @@ public class PlayerMovement : MonoBehaviour
 {
 
 
-    float playerSize = 1f;
+    // float playerSize = 1f;
+
+    public int controlSchemes;
+    public JumpHelper canJump;
 
     private float speed = 4f;
     //public PlayerMovement player; // awkward naming, probably should've named the class "Player"
     private Rigidbody2D playerBody;
+    private float defaultJumpPower = 6f;
+    private float strongJumpPower = 9f;
+    private float weakJumpPower = 4f;
+    private float timer;
 
 
     // Start is called before the first frame update
     void Start()
     {
         playerBody = GetComponent<Rigidbody2D>();
+        controlSchemes = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f); // movement
+        bool clearance = canJump.getJump(); // canJump is a helper, and getJump returns bool 
+                                            // checking if player is on the ground
+        timer += Time.deltaTime;
+        if (timer > 5f)
+        {
+            Debug.Log(Input.GetAxis("Horizontal"));
+            controlSchemes += 1;
+            if (controlSchemes > 4)
+            {
+                controlSchemes = 1;
+            }
+            timer = 0;
+        }
 
+        switch (controlSchemes)
+        {
+            case 1: // default controls
+                transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f); // movement
+                if (Input.GetButtonDown("Jump") & clearance)
+                {
+                    PlayerJump(defaultJumpPower);
+                }
+                break;
+            case 2:
+                transform.position -= new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f); // movement
+                if (Input.GetButtonDown("Jump") & clearance)
+                {
+                    PlayerJump(defaultJumpPower);
+                }
+                break;
+            case 3:
+                if(Input.GetButtonDown("Jump"))
+                {
+                    transform.position += new Vector3(speed * Time.deltaTime, 0f); // movement
+
+                }
+                if ((Input.GetAxis("Horizontal") > 0) & canJump.getJump())
+                {
+                    PlayerJump(strongJumpPower);
+                }
+                if ((Input.GetAxis("Horizontal") < 0))
+                {
+                    transform.position -= new Vector3(speed * Time.deltaTime, 0f); // movement
+
+                }
+                break;
+            case 4:
+                if (Input.GetButtonDown("Jump"))
+                {
+                    transform.position -= new Vector3(speed * Time.deltaTime, 0f); // movement
+
+                }
+                if ((Input.GetAxis("Horizontal") < 0) & canJump.getJump())
+                {
+                    PlayerJump(weakJumpPower);
+                }
+                if ((Input.GetAxis("Horizontal") > 0))
+                {
+                    transform.position -= new Vector3(speed * Time.deltaTime, 0f); // movement
+
+                }
+                break;
+        }
+                
     }
 
     public void PlayerJump(float power)
