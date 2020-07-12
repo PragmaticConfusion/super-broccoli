031c662f720c1ded0143775372e22d0c973e71de
PragmaticConfusion <10893930+PragmaticConfusion@users.noreply.github.com>
2020-07-12 19:58:05 +0530

    shitty preliminary movement

--- Assets/_Project/Scripts/PlayerMovement.cs
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -5,20 +5,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-//    private  player;
 
-    private float speed = 3f;
+    float playerSize = 1f;
+
+    public float jumpPower = 4f;
+    private float speed = 4f;
+
+    private Rigidbody2D playerBody;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f);
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f); // movement
 
+
+        // Raycast to check if player is close to ground to allow jump 
+
+        RaycastHit2D jumpCheck = Physics2D.Raycast(new Vector3(transform.position.x, 
+            transform.position.y - 0.5f), new Vector3(0, -1)); 
+
+        if (jumpCheck.distance < playerSize * 0.05f && playerBody.velocity.y < 0.05f)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                playerBody.velocity = new Vector3(playerBody.velocity.x, jumpPower * 1f);
+            }
+        }    
     }
 }
