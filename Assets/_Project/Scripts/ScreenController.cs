a249103a2ec724310bcd377fca9c42839a6b7600
Rahul Arasikere <arasikere.rahul@gmail.com>
2020-07-12 22:43:15 +0530

    added ui

---
+++ Assets/_Project/Scripts/ScreenController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenController : MonoBehaviour
+{
+    public GameObject startCanvas;
+    public GameObject pauseCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetUI();
+        startCanvas.gameObject.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        startCanvas.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pauseCanvas.SetActive(true);
+    }
+
+    public void ResumeGame(){
+        Time.timeScale = 1f;
+        pauseCanvas.SetActive(false);
+    }
+
+    void ResetUI()
+    {
+        startCanvas.gameObject.SetActive(false);
+        pauseCanvas.gameObject.SetActive(false);
+        Time.timeScale = 0f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+}
